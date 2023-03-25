e96be54c7509fac741850c60cd28ba20691b663c
Akash Singh <107348145+akash6062@users.noreply.github.com>
2023-03-25 18:48:03 +0530

    Update program to find the length of a string without using the library function stdio.h

--- program to find the length of a string without using the library function stdio.h
+++ program to find the length of a string without using the library function stdio.h
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-void main()
-
+int main()
 {
-
     char string[50];
-
     int i, length = 0;
 
- 
+    printf("Enter a string: ");
+    fgets(string, sizeof(string), stdin);
 
-    printf("Enter a string \n");
-
-    gets(string);
-
-   
-
-    for (i = 0; string[i] != '\0'; i++)		// keep going through each character of the string till its end
-
+    for (i = 0; string[i] != '\0'; i++)
     {
-
         length++;
-
     }
 
-    printf("The length of a string is the number of characters in it \n");
+    printf("The length of the string is %d\n", length);
 
-    printf("So, the length of %s = %d\n", string, length);
-
+    return 0;
 }
